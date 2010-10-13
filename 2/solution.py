5c20ae69f935e925e5d40193f0123a7d25eabeda
sztomi <cdev88@ymail.com>
2010-10-13 13:17:07 +0200

    prob2 in progress

---
+++ 2/solution.py
@@ -0,0 +1,25 @@
+from math import sqrt
+
+def fibo(n):
+	return int((1/sqrt(5))*pow(((1+sqrt(5))/2), n))
+
+
+f = 0
+x = 1
+summa = 0
+finished = False
+while not finished:
+	f = fibo(x)
+	x += 1
+	if f % 2 == 0: 
+		if f <= 4000000:
+			print f
+			summa += f
+		else:
+			finished = True
+	else:
+		print "> ", f
+
+print summa
+
+
